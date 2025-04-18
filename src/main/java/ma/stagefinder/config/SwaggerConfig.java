425791c994108dbf9cb0a2378910ae5fb7945c10
MALKI Mohammed <malki.mohammed.1997@gmail.com>
2025-04-18 11:14:24 +0200

    Ajout config Swagger

---
+++ src/main/java/ma/stagefinder/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package ma.stagefinder.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig
+{
+    @Bean
+    public OpenAPI openAPI()
+    {
+       // return new OpenAPI().info(new Info().title("Stage Finder API").version("1.0"));
+
+
+        return new OpenAPI().info(new Info().title("Stage Finder API").
+                description("Stage finder API").
+                contact(new Contact().name("seomaniak")).
+                version("1.0"));
+
+
+    }
+
+
+}
